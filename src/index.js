98e92857a796bc72409d6aa5b782d3dc0c3f13c7
Pravin Kumar <pravin.105111@gmail.com>
2019-01-22 00:09:24 +0530

    intial router set up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+import store from './redux/config-store';
 import './index.css';
 import App from './pages/app';
-import * as serviceWorker from './serviceWorker';
+import SignIn from './pages/sign-in';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        <Router history={createBrowserHistory}>
+            <Switch>
+                <Route path="/my" component={App} />
+                <Route path="/sign-in" component={SignIn} />
+            </Switch>
+        </Router>
+    </Provider>
+    , document.getElementById('root'));
---
+++ src/redux/config-store.js
@@ -0,0 +1,9 @@
+import {combineReducers, applyMiddleware, createStore} from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({
+
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+export default store;
