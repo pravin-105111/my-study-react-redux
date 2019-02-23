f418e81b7445919fc6ac5536a1f7da1f6a873788
Pravin Kumar <pravin.105111@gmail.com>
2019-02-23 14:07:17 +0530

    test case folder

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './pages/app';
+import App from '../pages/app';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
